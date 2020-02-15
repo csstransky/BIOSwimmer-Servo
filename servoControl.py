63f388ca3026945888634e3c2ae6cf733ad10362
Scott Yang <yang.xu3@husky.neu.edu>
2020-02-15 11:56:08 -0500

    Added a test file for moving servo

--- servoControl.py
+++ servoControl.py
@@ -3,19 +3,23 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 servoPIN = 11
 GPIO.setup(servoPIN, GPIO.OUT)
 pwm=GPIO.PWM(servoPIN, 50)
 pwm.start(0)
-
+print("start") 
 def SetAngle(angle):
-    duty = angle/18+2
-    GPIO.output(servoPIN,True)
-    pwm.ChangeDutyCycle(duty)
-    sleep(1)
-    GPIO.output(servoPIN,False)
-    pwm.ChangeDutyCycle(0)
-SetAngle(90)
+        duty = angle/18+2
+        GPIO.output(servoPIN,True)
+        print("test1")
+        pwm.ChangeDutyCycle(duty)
+        sleep(1)
+        GPIO.output(servoPIN,False)
+        print("test2")
+        pwm.ChangeDutyCycle(0)
+SetAngle(100)
+print ("stop")
 pwm.stop()
 GPIO.cleanup()
 
