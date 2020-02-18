bdbf199c531619793ca6c07cefd0bf66c397d64e
xuyang123123 <51219985+xuyang123123@users.noreply.github.com>
2020-02-18 16:41:47 -0500

    update readfile

--- servoControl.py
+++ servoControl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+# importing csv module 
+import csv 
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
@@ -8,13 +9,32 @@ GPIO.setup(servoPIN, GPIO.OUT)
 pwm=GPIO.PWM(servoPIN, 50)
 pwm.start(0)
 
+  
+# csv file name 
+filename = "aapl.csv"
+  
+# initializing the titles and rows list 
+fields = [] 
+rows = [] 
+  
+# reading csv file 
+with open(filename, 'r') as csvfile: 
+    # creating a csv reader object 
+    csvreader = csv.reader(csvfile) 
+      
+    # extracting field names through first row 
+    fields = csvreader.next() 
+  
+    # extracting each data row one by one 
+    for row in csvreader: 
+        rows.append(row) 
 def SetAngle(angle):
-    duty = angle/18+2
-    GPIO.output(servoPIN,True)
-    pwm.ChangeDutyCycle(duty)
-    sleep(1)
-    GPIO.output(servoPIN,False)
-    pwm.ChangeDutyCycle(0)
+  duty = angle/18+2
+  GPIO.output(servoPIN,True)
+  pwm.ChangeDutyCycle(duty)
+  sleep(1)
+  GPIO.output(servoPIN,False)
+  pwm.ChangeDutyCycle(0)
 SetAngle(90)
 pwm.stop()
 GPIO.cleanup()
